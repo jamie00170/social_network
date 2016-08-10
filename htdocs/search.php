91c00a9c8f6a3e4be31a376832893c7626d13f2a
Jamie Dale <jamiedd13@aol.com>
2016-08-10 17:57:35 +0100

    Add search page with basic search

---
+++ htdocs/search.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jamie
+ * Date: 10/08/2016
+ * Time: 17:31
+ */
+
+
+if (!empty($_GET['searchtext'])){
+
+    require("../database_connect.php");
+
+    $search_for = explode(" ", $_GET['searchtext']);
+
+    $first_name = $search_for[0];
+    $last_name = $search_for[1];
+
+    $q = "SELECT first_name, last_name FROM users WHERE first_name = '$first_name' AND last_name = '$last_name' ";
+
+    $r = @mysqli_query($dbc, $q);
+
+    while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+        echo $row['first_name'] . " " . $row['last_name'];
+    }
+
+    // have a form with hidden fields to contain a link to their profile??
+
+}else {
+    echo "no text input";
+}
