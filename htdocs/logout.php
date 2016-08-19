fb1f853a90772d1e9dd619abbbaf4b4b28a21a44
Jamie Dale <jamiedd13@aol.com>
2016-08-19 17:58:03 +0100

    Add logout.php

---
+++ htdocs/logout.php
@@ -0,0 +1,25 @@
+<link rel="stylesheet" href="../includes/styles.css">
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jamie
+ * Date: 08/08/2016
+ * Time: 10:19
+ */
+if (!isset($_COOKIE['username'])) {
+    require("login_functions.php");
+    redirect_user();
+} else {
+    // Delete the cookies
+    setcookie("username", "", time()-3600);
+    setcookie("first_name", "", time()-3600);
+}
+
+$page_title = "Logged out";
+include ("../includes/header.html");
+echo "<h1> Logged Out! </h1>
+<p>You are now logged out, {$_COOKIE
+['first_name']}!</p>";
+
+include ("../includes/footer.html");
+?>
